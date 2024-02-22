84876701cd97f039e735869c5fdb68040af2ffde
Sajda Kabir <sajdakabir@Sajdas-MacBook-Air.local>
2024-02-22 13:10:33 +0530

    hello rust

---
+++ hello_cargo/target/debug/deps/hello_cargo-631a5df1ed45a795.d
@@ -0,0 +1,5 @@
+/Users/sajdakabir/Downloads/Documents/learning/hacker_hub/hello_cargo/target/debug/deps/hello_cargo-631a5df1ed45a795: src/main.rs
+
+/Users/sajdakabir/Downloads/Documents/learning/hacker_hub/hello_cargo/target/debug/deps/hello_cargo-631a5df1ed45a795.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/sajdakabir/Downloads/Documents/learning/hacker_hub/hello_cargo/target/debug/hello_cargo: /Users/sajdakabir/Downloads/Documents/learning/hacker_hub/hello_cargo/src/main.rs
