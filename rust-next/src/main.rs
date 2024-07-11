9d07fb8b9f37c46f4f7c93b9c4b0e91f93e2ac69
sajdkabir <sajda.kbir@gmail.com>
2024-07-11 13:35:03 +0530

    added while and rev for loop

--- rust-next/src/main.rs
+++ rust-next/src/main.rs
@@ -11,62 +11,82 @@ let _greeting = String::from("hello world");
 let _name = String::from("Sajda");
 // println!("{}", name);
 
-conditionals
+// conditionals
 
-let is_even= false;
+// let is_even= false;
 
-if is_even {
-    println!("I am even")
-} else{
-    println!("I an odd")
-} 
-// another example of condition 
-let condition = true;
-    let number = if condition { 5 } else { 6 };
+// if is_even {
+//     println!("I am even")
+// } else{
+//     println!("I an odd")
+// } 
+// // another example of condition 
+// let condition = true;
+//     let number = if condition { 5 } else { 6 };
 
-    println!("The value of number is: {number}");
-
-
-// loops   --> rust has 3 types of loops -->> loop, while and for
+//     println!("The value of number is: {number}");
 
 
-
-for i in 0..10 {
-    println!("hey {}", i)
-}
-
-// example of loop
-
-loop {
-    println!("hey girl")    //--> need to stop it manuallly
-}
-
-// another exmaple
-
-let mut counter = 0;
-
-    let result = loop {
-        counter += 1;
-        println!("there am i {}", counter);
-
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-
-    println!("The result is {result}");
+// // loops   --> rust has 3 types of loops -->> loop, while and for
 
 
 
-let mut x= 0;
-let ans= loop {
-    x+=1;
-    if x==5 {
-        break x;
+// for i in 0..10 {
+//     println!("hey {}", i)
+// }
+
+// // example of loop
+
+// loop {
+//     println!("hey girl")    //--> need to stop it manuallly
+// }
+
+// // another exmaple
+
+// let mut counter = 0;
+
+//     let result = loop {
+//         counter += 1;
+//         println!("there am i {}", counter);
+
+//         if counter == 10 {
+//             break counter * 2;
+//         }
+//     };
+
+//     println!("The result is {result}");
+
+
+
+// let mut x= 0;
+// let ans= loop {
+//     x+=1;
+//     if x==5 {
+//         break x;
+//     }
+// };
+
+// println!("The ans is {ans}");
+
+
+// while loop
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
     }
-};
 
-println!("The ans is {ans}");
+    println!("LIFTOFF!!!");
 
+
+    // loop in rev
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
     
 }
