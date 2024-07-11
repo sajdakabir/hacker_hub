a46e178f479328d544473b7a8fa9201846ffa3c7
sajdkabir <sajda.kbir@gmail.com>
2024-07-11 13:10:07 +0530

    feat: string

--- rust-next/src/main.rs
+++ rust-next/src/main.rs
@@ -2,5 +2,16 @@ fn main() {
    let x = 10;
    print!("x: {}\n",x );
 
+//    string
+
+let greeting = String::from("hello world");
+println!("{}", greeting);
+
+
+let name = String::from("Sajda");
+println!("{}", name)
+
+
+
 
 }
