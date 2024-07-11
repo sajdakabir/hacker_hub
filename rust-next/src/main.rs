6040a964f0412d9cbf46c36339e4304cdcb1a010
sajdkabir <sajda.kbir@gmail.com>
2024-07-11 13:18:20 +0530

    feat: condition example

--- rust-next/src/main.rs
+++ rust-next/src/main.rs
@@ -20,5 +20,19 @@ if is_even {
 } else{
     println!("I an odd")
 } 
+// another example of condition 
+let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+
+
+// loops   --> rust has 3 types of loops -->> loop, while and for
+
+
+
+for i in 0..10 {
+    println!("hey {}", i)
+}
     
 }
