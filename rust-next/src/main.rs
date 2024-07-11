172249589f49362db84a695007e4471468aee7a9
sajdkabir <sajda.kbir@gmail.com>
2024-07-11 13:13:19 +0530

    feat: condition

--- rust-next/src/main.rs
+++ rust-next/src/main.rs
@@ -4,14 +4,21 @@ fn main() {
 
 //    string
 
-let greeting = String::from("hello world");
-println!("{}", greeting);
+let _greeting = String::from("hello world");
+// println!("{}", greeting);
 
 
-let name = String::from("Sajda");
-println!("{}", name)
+let _name = String::from("Sajda");
+// println!("{}", name);
 
+// conditionals
 
+let is_even= false;
 
-
+if is_even {
+    println!("I am even")
+} else{
+    println!("I an odd")
+} 
+    
 }
