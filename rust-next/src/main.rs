2aebba5ff2f6dd642d9f6ffbe7a5e5eb1ed252df
sajdkabir <sajda.kbir@gmail.com>
2024-07-23 14:14:29 +0530

    feat: browwing

--- rust-next/src/main.rs
+++ rust-next/src/main.rs
@@ -1,103 +1,103 @@
-fn main() {
-   let _x = 10;
-//    print!("x: {}\n",x );
+// fn main() {
+//    let _x = 10;
+// //    print!("x: {}\n",x );
 
-//    string
+// //    string
 
-let _greeting = String::from("hello world");
-// println!("{}", greeting);
-
-
-let _name = String::from("Sajda");
-// println!("{}", name);
-
-// conditionals
-
-let is_even= false;
-
-if is_even {
-    println!("I am even")
-} else{
-    println!("I an odd")
-} 
-// another example of condition 
-let condition = true;
-    let number = if condition { 5 } else { 6 };
-
-    println!("The value of number is: {number}");
+// let _greeting = String::from("hello world");
+// // println!("{}", greeting);
 
 
-// loops   --> rust has 3 types of loops -->> loop, while and for
+// let _name = String::from("Sajda");
+// // println!("{}", name);
+
+// // conditionals
+
+// let is_even= false;
+
+// if is_even {
+//     println!("I am even")
+// } else{
+//     println!("I an odd")
+// } 
+// // another example of condition 
+// let condition = true;
+//     let number = if condition { 5 } else { 6 };
+
+//     println!("The value of number is: {number}");
 
 
-
-for i in 0..10 {
-    println!("hey {}", i)
-}
-
-// example of loop
-
-loop {
-    println!("hey girl")    //--> need to stop it manuallly
-}
-
-// another exmaple
-
-let mut counter = 0;
-
-    let result = loop {
-        counter += 1;
-        println!("there am i {}", counter);
-
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-
-    println!("The result is {result}");
+// // loops   --> rust has 3 types of loops -->> loop, while and for
 
 
 
-let mut x= 0;
-let ans= loop {
-    x+=1;
-    if x==5 {
-        break x;
-    }
-};
+// for i in 0..10 {
+//     println!("hey {}", i)
+// }
 
-println!("The ans is {ans}");
+// // example of loop
+
+// loop {
+//     println!("hey girl")    //--> need to stop it manuallly
+// }
+
+// // another exmaple
+
+// let mut counter = 0;
+
+//     let result = loop {
+//         counter += 1;
+//         println!("there am i {}", counter);
+
+//         if counter == 10 {
+//             break counter * 2;
+//         }
+//     };
+
+//     println!("The result is {result}");
 
 
-// while loop
 
-    let mut number = 3;
+// let mut x= 0;
+// let ans= loop {
+//     x+=1;
+//     if x==5 {
+//         break x;
+//     }
+// };
 
-    while number != 0 {
-        println!("{number}!");
-
-        number -= 1;
-    }
-
-    println!("LIFTOFF!!!");
+// println!("The ans is {ans}");
 
 
-    // loop in rev
+// // while loop
 
-    for number in (1..4).rev() {
-        println!("{number}!");
-    }
-    println!("LIFTOFF!!!");
+//     let mut number = 3;
 
-// Functuons
+//     while number != 0 {
+//         println!("{number}!");
 
-hey_i_am_function();
+//         number -= 1;
+//     }
 
-println!("i am the five function {}", five());
+//     println!("LIFTOFF!!!");
 
-println!("I am the sum {}", sum(3,5));
+
+//     // loop in rev
+
+//     for number in (1..4).rev() {
+//         println!("{number}!");
+//     }
+//     println!("LIFTOFF!!!");
+
+// // Functuons
+
+// hey_i_am_function();
+
+// println!("i am the five function {}", five());
+
+// println!("I am the sum {}", sum(3,5));
     
-}
+// }
 
 fn hey_i_am_function() {
     println!("hello")
@@ -113,4 +113,22 @@ fn five() -> i32{
 
 fn sum(x:i32, y:i32) -> i32 {
     x+y
+}
+
+// memory management
+fn testing(){
+    //declear a string
+    let s1= String::from("hello world");
+}
+
+
+
+
+fn main(){
+    let mut s1= String::from("hey ");
+    update_word(&mut s1);
+    println!("{}",s1)
+}
+fn update_word(s:&mut String){
+    s.push_str("there")
 }
