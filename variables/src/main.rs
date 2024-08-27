018b7da08f4e35529261bd3a257be35581f5a24c
sajdkabir <sajda.kbir@gmail.com>
2024-08-27 10:34:05 +0530

    feat:Numeric Operations

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,16 +1,27 @@
+
 fn main() {
-  println!("hey there");
-  // mutation
-  let mut age = 10;
+// addition
+let sum= 4+5;
+println!("the sum is {sum}");
 
-  age =10+1;
-  println!("age is {age}");
-  // shadowing 
+// subtraction
+let sub = 4-5;
+println!("the sub is {sub}");
 
-  let age = age;
+// divition 
 
-  println!("age is now {age}");
-  const PI: f32 = 3.14;
+let div = 4/2;
+println!("the div is {div}");
 
-  println!("this is a const {}", PI);
+// truncated
+let div = 5/2;
+println!("the div is {div}");
+
+
+let div = 5.0/2.0;
+println!("the div is {div}");
+
+// truncated
+let div = 5_f32/2_f32;
+println!("the div is {div}");
 }
