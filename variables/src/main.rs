8239e21c1dd5bc4e79cd6040d152f9aa9c2b6ed4
sajdkabir <sajda.kbir@gmail.com>
2024-08-28 11:15:15 +0530

    feat:tuple

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -24,4 +24,21 @@ println!("the div is {div}");
 // truncated
 let div = 5_f32/2_f32;
 println!("the div is {div}");
+
+// char
+
+// let c = 's';
+
+// string
+// let s= "sajda";
+
+// tuple
+
+let tup =(10, true, 10.5);
+let (x, y, z)= tup;
+println!("x is {x}");
+println!("y is {y}");
+println!("z is {z}");
+println!("0th value is {}", tup.0);
+let unit_tup=();
 }
