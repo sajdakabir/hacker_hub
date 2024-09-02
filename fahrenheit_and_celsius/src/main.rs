edca5ca2ce083867786736b32cbbeb72d7aa3b3f
sajdkabir <sajda.kbir@gmail.com>
2024-09-02 10:29:21 +0530

    feat: Convert temperatures between Fahrenheit and Celsius!

---
+++ fahrenheit_and_celsius/src/main.rs
@@ -0,0 +1,26 @@
+use std::io;
+
+fn main() {
+    println!("Convert temperatures between Fahrenheit and Celsius!");
+
+    println!("Enter a temperature in Celsius: ");
+
+    let mut cel= String::new();
+    io::stdin()
+        .read_line(&mut cel)
+        .expect("Failed to read value");
+
+
+    // need to convert value
+    let cel: f32= match cel.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please enter a number!");
+            return;
+        },
+    };
+
+println!("Enter value is {cel}");
+
+    
+}
