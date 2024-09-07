27f0172f214644abb7e544f654aa395b65e9af99
sajdkabir <sajda.kbir@gmail.com>
2024-09-07 11:39:06 +0530

    feat: Borrowing rules

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,32 +1,11 @@
 fn main() {
-    // let s1 = String::from("Hello");
-    // let s2= s1.clone(); //it'v very expensive
-    // let s2= s1;
-    
-
-    // println!("S= {s1}");
 
     let mut s = String::from("Hello");
-    // take_ownership(s);
-    // println!("S= {s}");
+    let r1= &s;
+    let r2= &s;
+    println!("r1= {r1} , r2 ={r2},");
+    let r3= &mut s;
+    // println!("r1= {r1} , r2 ={r2},r3={r3}");
+    println!("r3={r3}");
 
-    // let s= give_ownership();
- takes_and_gives_ownership(&mut s);
-}
-
-// fn take_ownership (s: String){
-//     println!("This is from take ownership {s}.")
-// }
-
-// fn give_ownership() -> String{
-     
-//     let s= String::from(" World");
-//     s
-
-// }
-
-fn takes_and_gives_ownership(s: &mut String){
-    let len= s.len();
-    s.push_str("bye");
-    println!("s ={s} and len is {len}");
 }
