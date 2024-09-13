63dcde7dc081508d2e0964658751d9acb9a1b544
sajdkabir <sajda.kbir@gmail.com>
2024-09-13 09:28:01 +0530

    normal calculate_rectangle_area

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,40 +1,53 @@
-struct User {
-    username: String,
-    email: String,
-    active:bool,
-    sing_in_ount: u64
-}
-struct Color (u8, u8, u8);
+// struct User {
+//     username: String,
+//     email: String,
+//     active:bool,
+//     sing_in_ount: u64
+// }
+// struct Color (u8, u8, u8);
 
-struct Point (u8, u8, u8);
+// struct Point (u8, u8, u8);
+// fn main() {
+
+//     let mut user_1 = build_user(String::from("saju"), String::from("gmail"));
+//     println!("the value of userName {}", user_1.username);
+//     user_1.username.push_str(" kabir");
+//     println!("the value of userName {}", user_1.username);
+
+//     let user_2 = User {
+//         username: String::from("saju"),
+//         ..user_1
+//     };
+//     println!("the value {}", user_2.active);
+
+//     let black = Color(0,0,0);
+//     let white= Color(0,0,0);
+
+//     set_bg_color(black);
+// }
+
+// fn set_bg_color(color: Color){
+//     println!("{} {} {}", color.0, color.1, color.2);
+// }
+
+// fn build_user(username: String, email: String)-> User {
+//     User {
+//     username,
+//     email,
+//     active: true,
+//     sing_in_ount:1
+//    }
+// }
+
+
+
 fn main() {
+    println!("hey there");
+   let area =calculate_rectangle_area(2, 2);
+    println!("the value of area {}", area);
 
-    let mut user_1 = build_user(String::from("saju"), String::from("gmail"));
-    println!("the value of userName {}", user_1.username);
-    user_1.username.push_str(" kabir");
-    println!("the value of userName {}", user_1.username);
-
-    let user_2 = User {
-        username: String::from("saju"),
-        ..user_1
-    };
-    println!("the value {}", user_2.active);
-
-    let black = Color(0,0,0);
-    let white= Color(0,0,0);
-
-    set_bg_color(black);
 }
 
-fn set_bg_color(color: Color){
-    println!("{} {} {}", color.0, color.1, color.2);
-}
-
-fn build_user(username: String, email: String)-> User {
-    User {
-    username,
-    email,
-    active: true,
-    sing_in_ount:1
-   }
+fn calculate_rectangle_area(h: u32, w: u32) -> u32{
+    h*w
 }
