1c0a6160b970dfcc94fde2bd644607b1293d0c88
sajdkabir <sajda.kbir@gmail.com>
2024-09-16 21:11:38 +0530

    more about method

---
+++ method/src/main.rs
@@ -0,0 +1,41 @@
+
+
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    fn area(&self)-> u32{
+        self.width*self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+         self.width>=other.width && self.height>=other.height 
+    }
+}
+
+
+
+
+fn main() {
+
+    let rect = Rectangle{
+        width:20,
+        height: 20
+    };
+
+    let rect1 = Rectangle {
+        width: 90,
+        height: 90
+        
+    };
+
+    println!("the area of the rectangle is {}", rect.area());
+
+    println!("can rect hold rect1 {}", rect.can_hold(&rect1));
+    println!("can rect1 hold rect {}", rect1.can_hold(&rect));
+
+
+   
+}
