a34b2c9b45ba2504783ed480bca891b9d22d2641
sajdkabir <sajda.kbir@gmail.com>
2024-09-19 13:35:22 +0530

    option and match

--- match_rust/src/main.rs
+++ match_rust/src/main.rs
@@ -5,6 +5,9 @@ fn main() {
 
 let x= None;
 println!("the value is {:?} ", plase_one(x));
+println!("the value is {:?} ",sum(4, Some(4)));
+println!("the value is {:?} ",sum(4, None));
+
 
 }
 
@@ -13,4 +16,12 @@ fn plase_one(x:Option<i32>)-> Option<i32>{
         Some(i)=>Some(i+1),
         None=>None
     }
+}
+
+fn sum (x: i32, y : Option<i32>)-> i32{
+    match y {
+        Some(i)=> x+i,
+        None=> x
+    }
+
 }
