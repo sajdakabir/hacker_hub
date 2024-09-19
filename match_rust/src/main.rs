3dc9015afc8d788112b7a330b59d07e4b92b954b
sajdkabir <sajda.kbir@gmail.com>
2024-09-19 13:31:20 +0530

    option match

--- match_rust/src/main.rs
+++ match_rust/src/main.rs
@@ -1,40 +1,16 @@
-#[derive(Debug)] // so we can inspect the state in a minute
-enum UsState {
-    Alabama,
-    Alaska
-}
 
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
 
 fn main() {
    
-//    let coin = Coin::Penny;
-//    let coin = Coin::Quarter(UsState::Alabama);
-   let coin = Coin::Quarter(UsState::Alaska);
-    let x= get_coin(coin);
-    println!("the {x}");
+
+let x= None;
+println!("the value is {:?} ", plase_one(x));
+
 }
 
-fn get_coin(coin:Coin)->u8{
-    match coin {
-        Coin::Penny=> {
-            println!("this is prnny");
-            1
-        },
-        Coin::Nickel=>5,
-        Coin::Dime=>10,
-        Coin::Quarter(UsState::Alaska)=>{
-            println!("this hi from Alaska");
-            25
-        },
-        Coin::Quarter(state)=>{
-            println!("the Q is {state:?}");
-            25
-        }
+fn plase_one(x:Option<i32>)-> Option<i32>{
+    match x {
+        Some(i)=>Some(i+1),
+        None=>None
     }
 }
