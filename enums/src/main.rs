1cf437be37b354468c17d28ce295d8cc32bb1d5a
sajdkabir <sajda.kbir@gmail.com>
2024-09-19 11:19:37 +0530

    emun with type

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -2,25 +2,16 @@
 
 #[derive(Debug)]
 enum KindIp {
-    V4,
-    V6
-}
-struct IpAddress{
-    kind: KindIp,
-    add: String
+    V4(String),
+    V6(String)
 }
 
-impl IpAddress {
-    fn new(add: &str)-> Self{
-        Self { kind: KindIp::V4, add: add.to_string() }
-    }
-}
 
 fn main() {
-    let google =IpAddress::new("0.0.0.0");
-    get_ip_add(google);
+    let x= KindIp::V4(String::from("hfd"));
+    get_ip_add(x);
 }
 
-fn get_ip_add(ip:IpAddress){
-    println!("the ip add is {} {:?}", ip.add, ip.kind);
+fn get_ip_add(ip:KindIp){
+    println!("the ip add is {:?}", ip);
 }
