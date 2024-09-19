c9755062eaca2080516c4fc8a829b7dcd78afd16
sajdkabir <sajda.kbir@gmail.com>
2024-09-19 11:44:21 +0530

    option

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,17 +1,33 @@
 
 
-#[derive(Debug)]
-enum KindIp {
-    V4(String),
-    V6(String)
+// #[derive(Debug)]
+// enum KindIp {
+//     V4(String),
+//     V6(String)
+// }
+
+
+// fn main() {
+//     let x= KindIp::V4(String::from("hfd"));
+//     get_ip_add(x);
+// }
+
+// fn get_ip_add(ip:KindIp){
+//     println!("the ip add is {:?}", ip);
+// }
+
+enum Message {
+    Quit,
+    Move{x:i32, y:i32},
+    Write(String),
+    ChnaageColor(i32, i32,i32)
 }
-
-
 fn main() {
-    let x= KindIp::V4(String::from("hfd"));
-    get_ip_add(x);
-}
+    let op = Option::Some(1);
 
-fn get_ip_add(ip:KindIp){
-    println!("the ip add is {:?}", ip);
+    let x= 2;
+    let sum= x+op.unwrap();
+    let sum1= x+op.unwrap_or(0);
+
+    let anyType= Option::Some(3.4);
 }
