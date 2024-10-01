bbc905cd8db52f3e3b3fe7e5590c448773663087
sajdkabir <sajda.kbir@gmail.com>
2024-10-01 12:16:03 +0530

    Overwriting a Value

--- hash_map/src/main.rs
+++ hash_map/src/main.rs
@@ -1,22 +1,32 @@
 use std::collections::HashMap;
 
 fn main() {
-    let mut scores = HashMap::new();
-    let team_bule= "bule".to_string();
-    let team_red= "red".to_string();
-    scores.insert(team_bule, 10);
-    scores.insert(team_red, 50);
+    // let mut scores = HashMap::new();
+    // let team_bule= "bule".to_string();
+    // let team_red= "red".to_string();
+    // scores.insert(team_bule, 10);
+    // scores.insert(team_red, 50);
 
-    println!("the valu of hash map id  {:?}", scores);
+    // println!("the valu of hash map id  {:?}", scores);
 
     //get map value
 
     // let value = scores.get(&String::from("red")).unwrap_or(&0);
-    let value = scores.get(&String::from("red")).copied().unwrap_or(0);
+    // let value = scores.get(&String::from("red")).copied().unwrap_or(0);
 
-    println!("the value is {value}");
+    // println!("the value is {value}");
 
-    for (key, val) in &scores{
-        println!("key= {key} and value= {val}");
-    }
+    // for (key, val) in &scores{
+    //     println!("key= {key} and value= {val}");
+    // }
+
+
+    // Overwriting a Value
+    let mut scores = HashMap::new();
+
+    scores.insert("hi".to_string(), 10);
+    scores.insert("hi".to_string(), 45);
+
+    println!("the value {:?}", scores);
+
 }
