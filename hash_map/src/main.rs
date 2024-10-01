d8b0345a9799d09bfd88c5856916ddc2ba4f1e04
sajdkabir <sajda.kbir@gmail.com>
2024-10-01 12:31:10 +0530

    Updating a Value Based on the Old Value

--- hash_map/src/main.rs
+++ hash_map/src/main.rs
@@ -36,4 +36,18 @@ fn main() {
 
     println!("the value {:?}", scores);
 
+    // Updating a Value Based on the Old Value
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+
+        *count+=1;
+    }
+
+    println!("{map:?}");
+
 }
