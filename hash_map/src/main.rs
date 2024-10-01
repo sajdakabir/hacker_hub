6d3182e8833142c0a22f8ec89c18629b1447e74a
sajdkabir <sajda.kbir@gmail.com>
2024-10-01 12:00:41 +0530

    get map value

--- hash_map/src/main.rs
+++ hash_map/src/main.rs
@@ -2,8 +2,17 @@ use std::collections::HashMap;
 
 fn main() {
     let mut scores = HashMap::new();
-    scores.insert("bule".to_string(), 10);
-    scores.insert("red".to_string(), 50);
+    let team_bule= "bule".to_string();
+    let team_red= "red".to_string();
+    scores.insert(team_bule, 10);
+    scores.insert(team_red, 50);
 
     println!("the valu of hash map id  {:?}", scores);
+
+    //get map value
+
+    // let value = scores.get(&String::from("red")).unwrap_or(&0);
+    let value = scores.get(&String::from("red")).copied().unwrap_or(0);
+
+    println!("the value is {value}");
 }
