6e8e755801eb1ea4b394caa8f505f16c62d03315
sajdkabir <sajda.kbir@gmail.com>
2024-10-03 08:20:06 +0530

    read user name code

--- more_about_result/src/main.rs
+++ more_about_result/src/main.rs
@@ -1,20 +1,56 @@
-use std::fs::File;
-use std::io::ErrorKind;
+// use std::fs::File;
+// use std::io::{self, ErrorKind, Read};
 
-fn main() {
-    let greeting_file_result = File::open("hello.txt");
+// fn main() {
+//     let greeting_file_result = File::open("hello.txt");
 
-    let greeting_file = match greeting_file_result{
-        Ok(file)=> file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound=> match File::create("hello.txt"){
-                Ok(file)=>file,
-                Err(e) => panic!("Problem creating the file: {e:?}"),
-            },
-            other_error => {
-                panic!("Problem opening the file: {other_error:?}");
-            }
-        }
-    };
+//     let greeting_file = match greeting_file_result{
+//         Ok(file)=> file,
+//         Err(error) => match error.kind() {
+//             ErrorKind::NotFound=> match File::create("hello.txt"){
+//                 Ok(file)=>file,
+//                 Err(e) => panic!("Problem creating the file: {e:?}"),
+//             },
+//             other_error => {
+//                 panic!("Problem opening the file: {other_error:?}");
+//             }
+//         }
+//     };
     
+// }
+
+// fn read_username_from_file()-> Result<String, io::Error>{
+//     let username_file_result = File::open("hello.txt");
+//     let mut username_file = match read_username_from_file() {
+//         Ok(file)=> file,
+//         Err(e)=> return Err(e),
+//     };
+//     let username= String::new;
+//     match username_file.read_to_string(&mut username) {
+//         Ok(_) => Ok(username),
+//         Err(e) => Err(e),
+//     }
+// }
+
+use std::fs::File;
+use std::io::{self, Read};
+
+fn main (){
+    
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
 }
