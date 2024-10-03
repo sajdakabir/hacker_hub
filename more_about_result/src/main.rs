51b918980896ebcea170e42d968e8fcf7562973b
sajdkabir <sajda.kbir@gmail.com>
2024-10-03 08:22:36 +0530

    the ? Operator

--- more_about_result/src/main.rs
+++ more_about_result/src/main.rs
@@ -25,7 +25,7 @@
 //         Ok(file)=> file,
 //         Err(e)=> return Err(e),
 //     };
-//     let username= String::new;
+//     let mut username= String::new;
 //     match username_file.read_to_string(&mut username) {
 //         Ok(_) => Ok(username),
 //         Err(e) => Err(e),
@@ -36,21 +36,20 @@ use std::fs::File;
 use std::io::{self, Read};
 
 fn main (){
-    
+
 }
 
-fn read_username_from_file() -> Result<String, io::Error> {
-    let username_file_result = File::open("hello.txt");
 
-    let mut username_file = match username_file_result {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
+// A Shortcut for Propagating Errors: the ? Operator
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
 
     let mut username = String::new();
 
-    match username_file.read_to_string(&mut username) {
-        Ok(_) => Ok(username),
-        Err(e) => Err(e),
-    }
+     username_file.read_to_string(&mut username)?;
+     Ok(username)
 }
+
+
+
