9f708c5e88c79bc7662190cfe7f02871854a92c0
sajdkabir <sajda.kbir@gmail.com>
2024-10-05 19:25:13 +0530

    struct

--- grnerics/src/main.rs
+++ grnerics/src/main.rs
@@ -1,3 +1,9 @@
+
+struct Point <T>{
+    x: T,
+    y: T
+}
+
 fn main() {
 
     let list = vec![1,34,5,2,3,4];
@@ -7,6 +13,16 @@ fn main() {
 
     println!("the value is {r}");
     println!("the value is {r1}");
+
+    let integer = Point{
+        x: 3,
+        y: 4
+    };
+    
+    let flote = Point{
+        x: 3.9,
+        y: 4.0
+    };
     
 }
 
